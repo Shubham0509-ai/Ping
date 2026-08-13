82e5a3c34ea70ca5a7022b12f304bf64abf3af5a
Shubham0509-ai <shubhamsharmaxyz12@gmail.com>
2026-08-13 23:48:29 +0530

    Initialise project

---
+++ backend/src/server.js
@@ -0,0 +1,15 @@
+import express from "express";
+import dotenv from "dotenv";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+dotenv.config({ path: "./.env" });
+
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+
+app.listen(PORT, () => console.log("Server running on port: " + PORT));
