518695851d8763558c29416b8070a63813d81da4
Shubham0509-ai <shubhamsharmaxyz12@gmail.com>
2026-08-14 23:57:36 +0530

    Set up server and add package.json for project configuration

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,10 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
 
 const app = express();
+const __dirname = path.resolve();
+
 const PORT = process.env.PORT || 3000;
 
 dotenv.config({ path: "./.env" });
@@ -11,5 +14,14 @@ import messageRoutes from "./routes/message.route.js";
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+
+// make ready for deployment
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")))
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
+    })
+}
 
 app.listen(PORT, () => console.log("Server running on port: " + PORT));
